88700f80b979fcea7cd57cd01d45bd86ab8d4c5b
mjunior <mauriciojs.junior@gmail.com>
2017-09-12 01:22:32 +0000

    EX02 Estruturas de controle

---
+++ examples/ex02/ex02.rb
@@ -0,0 +1,14 @@
+nota = 10
+if nota > 7
+  puts "Boa nota!"
+else
+  puts "Nota ruim!"
+end
+
+puts "===================================="
+# estruturas de controle: for
+for i in (1..3)
+  puts i
+  x = i
+end
+puts x
